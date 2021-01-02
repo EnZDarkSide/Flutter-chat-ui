a500e66b2af7f19fc332a58c113843d6bff3b59a
EnZDarkSide <vladyavorsky478@gmail.com>
2021-01-02 08:41:03 +0500

    Added search field

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,51 +11,83 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var searchPressed = false;
+  var _controller = TextEditingController();
+
+  _searchField() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: TextField(
+        autofocus: true,
+        controller: _controller,
+        textCapitalization: TextCapitalization.sentences,
+        onChanged: (value) {},
+        decoration: InputDecoration.collapsed(hintText: 'Send a message...'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(
-          leading: IconButton(
-              icon: Icon(Icons.menu),
-              iconSize: 30.0,
-              color: Colors.white,
-              onPressed: () {}),
-          title: Text(
-            'Chats',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          elevation: 0.0,
-          actions: [
-            IconButton(
-                icon: Icon(Icons.search),
+    return GestureDetector(
+      onTap: () {
+        searchPressed = false;
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+          backgroundColor: Theme.of(context).primaryColor,
+          appBar: AppBar(
+            leading: IconButton(
+                icon: Icon(Icons.menu),
                 iconSize: 30.0,
                 color: Colors.white,
                 onPressed: () {}),
-          ],
-        ),
-        body: Column(
-          children: [
-            CategorySelector(),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Theme.of(context).accentColor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    )),
-                height: 500,
-                child: Column(
-                  children: [FavoriteContacts(), RecentChats()],
+            title: !searchPressed
+                ? Text(
+                    'Chats',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                : _searchField(),
+            elevation: 0.0,
+            actions: [
+              IconButton(
+                  icon: Icon(Icons.search),
+                  iconSize: 30.0,
+                  color: Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      searchPressed = !searchPressed;
+                    });
+                  }),
+            ],
+          ),
+          body: Column(
+            children: [
+              CategorySelector(),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      )),
+                  height: 500,
+                  child: Column(
+                    children: [FavoriteContacts(), RecentChats()],
+                  ),
                 ),
-              ),
-            )
-          ],
-        ));
+              )
+            ],
+          )),
+    );
   }
 }
